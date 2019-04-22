4c4e1764b0bdcd761993e4fd80d79216cecac923
Neche <adam@adamkarst.com>
2019-04-21 21:33:20 -0600

    Moving to decorative lines in content pane. JS samples included for this.

--- app.js
+++ app.js
@@ -69,7 +69,3 @@ app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function () {
     debug('Express server listening on port ' + server.address().port);
 });
-
-app.get('/#services', function (req, res) {
-    res.render('users');
-});
